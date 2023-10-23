8ded7d98a1006db5e3837bc9713cee6233d0a7e7
JulianCastill0 <juli.castillo909@gmail.com>
2023-10-23 13:06:33 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,90 @@
+const URL = 'https://japceibal.github.io/japflix_api/movies-data.json';
+
+//* ====== Elementos de DOM ====== *//
+
+const lista = document.querySelector('#lista'),
+	inputBuscar = document.querySelector('#inputBuscar'),
+	btn = document.querySelector('#btnBuscar'),
+	listItem = document.getElementsByClassName('list-item'),
+	canvas_title = document.getElementById("offcanvasTopLabel"),
+ 	canvas_body = document.getElementById("offcanvas-body"),
+	canvas_lista_more = document.getElementById("listaMore");
+
+//* ====== Eventos ====== *//
+
+document.addEventListener('DOMContentLoaded', () => {
+	let peliculasArr = new Array();
+	fetch(URL)
+		.then((response) => response.json())
+		.then((data) => {
+			data.forEach((pelicula) => {
+				const li = document.createElement('li');
+				li.setAttribute('hidden', true);
+				li.setAttribute('data-bs-toggle', 'offcanvas');
+				li.setAttribute('data-bs-target', '#offcanvasTop');
+				li.setAttribute('aria-controls', 'offcanvasTop');
+				li.addEventListener("click", () => {
+					canvas_title.innerHTML = pelicula.title;
+					canvas_body.innerHTML = `${pelicula.overview}<hr>`;
+					for (let e of pelicula.genres){
+						let generos = JSON.stringify(e["name"]);
+						if (pelicula.genres.indexOf(e) === 2){
+							canvas_body.innerHTML += generos.slice(1, -1);
+						} else {
+							canvas_body.innerHTML += generos.slice(1, -1) + " - ";
+						};
+					};
+					let listaDatosPelicula = document.getElementsByClassName("lista-item");
+					listaDatosPelicula[0].innerHTML = `<span>Year:</span> <span>${pelicula.release_date.split("-")[0]}</span>`;
+					listaDatosPelicula[1].innerHTML = `<span>Runtime:</span> <span>${pelicula.runtime} mins</span>`;
+					listaDatosPelicula[2].innerHTML = `<span>Budget:</span> <span>$${pelicula.budget}</span>`;
+					listaDatosPelicula[3].innerHTML = `<span>Revenue:</span> <span>$${pelicula.revenue}</span>`;
+				});
+				li.classList.add('list-item');
+				li.innerHTML = `
+        <div class='data-content'>
+          <h3>${pelicula.title}</h3> 
+          <p>${pelicula.tagline}</p>
+        </div>
+        <div class='star-content'>
+					<span class="fa fa-star"></span>
+					<span class="fa fa-star"></span>
+					<span class="fa fa-star"></span>
+					<span class="fa fa-star"></span>
+					<span class="fa fa-star"></span>
+        </div>`;
+
+				lista.appendChild(li);
+
+				const stars = li.querySelectorAll('.fa-star');
+				for (let i = 0; i < (pelicula.vote_average - 1) / 2; i++) {
+					stars[i].classList.add('checked');
+				}
+				peliculasArr.push(pelicula);
+			});
+		});
+
+	const buscarBtnFunc = () => {
+		const valueInputBuscar = inputBuscar.value.toLowerCase();
+		peliculasArr.forEach((pelicula) => {
+			if (
+				pelicula.title.toLowerCase().includes(valueInputBuscar) ||
+				pelicula.tagline.toLowerCase().includes(valueInputBuscar) ||
+				pelicula.overview.toLowerCase().includes(valueInputBuscar) ||
+				pelicula.genres.some((genero) => genero.name.toLowerCase().includes(valueInputBuscar))
+			) {
+				listItem[peliculasArr.indexOf(pelicula)].removeAttribute('hidden');
+			} else {
+				listItem[peliculasArr.indexOf(pelicula)].setAttribute('hidden', true);
+			}
+		});
+	};
+
+	btn.addEventListener('click', buscarBtnFunc);
+
+	window.addEventListener('keydown', (e) => {
+		if (e.keyCode === 13) {
+			buscarBtnFunc();
+		}
+	});
+});
